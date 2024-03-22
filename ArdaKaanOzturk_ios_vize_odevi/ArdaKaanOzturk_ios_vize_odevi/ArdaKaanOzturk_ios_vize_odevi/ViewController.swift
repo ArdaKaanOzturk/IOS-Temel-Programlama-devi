9e296459acc62f23598defca73f1d56a922eae46
Arda Kaan Öztürk <123774817+ArdaKaanOzturk@users.noreply.github.com>
2024-03-23 00:10:53 +0300

    Add files via upload

---
+++ ArdaKaanOzturk_ios_vize_odevi/ArdaKaanOzturk_ios_vize_odevi/ArdaKaanOzturk_ios_vize_odevi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArdaKaanOzturk_ios_vize_odevi
+//
+//  Created by Arda Kaan Öztürk on 22.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
